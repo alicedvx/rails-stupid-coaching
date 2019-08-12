4d27333f10781278dae34287a60af8a3d64a0f47
Alice Deveaux <alice.dvx@icloud.com>
2019-08-12 11:27:09 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'questions/ask'
+  get 'questions/answer'
+  get 'pages/ask'
+  get 'pages/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
