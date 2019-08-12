dbf9e50191fcf84da5ec0fdc8480a9493f60df70
Alice Deveaux <alice.dvx@icloud.com>
2019-08-12 14:52:31 +0200

    finished stupid coaching on Rails (without optional)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @questions = params[:question]
+    if @questions.include? "?"
+      @answer = "Silly question, get back to work!"
+    elsif @questions == "I'm going to work"
+      @answer = "Great!"
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def show
+    @answer = params[:question]
   end
 end
